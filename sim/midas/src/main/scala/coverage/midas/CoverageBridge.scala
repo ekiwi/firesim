c0100caa612c8fdbe0f5557bc57996dfdf828849
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-05-04 16:11:47 -0700

    CoverageBridge: try to collect coverage after N cycles

--- sim/midas/src/main/scala/coverage/midas/CoverageBridge.scala
+++ sim/midas/src/main/scala/coverage/midas/CoverageBridge.scala
@@ -24,17 +24,49 @@ class CoverageBridgeModule(key: CoverageBridgeKey)(implicit p: Parameters) exten
     val io = IO(new WidgetIO())
     val hPort = IO(HostPort(new CoverageBundle(key.counterWidth)))
 
-    // keep scan chain disables for now
+    // keep scan chain disabled for now
     hPort.toHost.hReady := true.B
 
     // ignore scanchain output for now
     hPort.fromHost.hValid := true.B
     hPort.hBits.cover_en := false.B
 
-    // count simulation cycles
+    // count simulation cycles (this is just to learn more about bridges)
     val simCycles = RegInit(0.U(64.W))
     when(hPort.toHost.fire()) { simCycles := simCycles + 1.U }
-    genROReg(simCycles, "sim_cycles")
+    genROReg(simCycles(31,0), "sim_cycles")
+    genROReg(simCycles(63,32), "sim_cycles_2")
+
+
+    // we are going to clock out the coverage after N cycles
+    val CoverageAt = 4000
+    // the cover counter counts how long we enable to scan chain for
+    val coverCounter = RegInit(0.U(32.W))
+    when(hPort.fromHost.fire() && hPort.hBits.cover_en) {
+      coverCounter := coverCounter + 1.U
+    }
+    val scanning = RegInit(false.B)
+    // when we reach cycle N, we switch into scanning mode
+    when(simCycles === CoverageAt.U) { scanning := true.B; coverCounter := 0.U }
+    when(scanning) {
+      // in scanning mode we keep the scan chain enabled until we have scanned out everything
+      hPort.hBits.cover_en := true.B
+      when(coverCounter === key.covers.length.U) {
+        scanning := false.B
+      }
+    }
+
+    // use a queue that is big enough to contain all cover counts
+    val nextPowerOfTwo = BigInt(1) << log2Ceil(key.covers.length)
+    val counterType = UInt(key.counterWidth.W)
+    val counts = Module(new Queue(counterType, nextPowerOfTwo.toInt))
+    counts.io.enq.bits := hPort.hBits.cover_out
+    counts.io.enq.valid := scanning && hPort.toHost.fire()
+    // we don't empty the queue yet :(
+    counts.io.deq.ready := false.B
+
+    genROReg(counts.io.count, "cover_data_available")
+
 
     val test = 1234567.U(32.W)
     genROReg(test, "cover_test")
